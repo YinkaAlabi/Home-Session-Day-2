1ae400a8e633fae8c94da4b95c4df22e1032a9a2
Yinka Alabi <adeyinka.alabi.a@gmail.com>
2017-02-14 10:22:47 +0100

    Added extra test cases for reverseString and a few comments in the implementation

--- Andelabs/reverse string lab/index.js
+++ Andelabs/reverse string lab/index.js
@@ -1,9 +1,14 @@
 function reverseString(word){
+	if(typeof word !== "string"){					//check if argument is a string
+		return 'false argument';
+	}
+
 	if (word === ''){
 		return null;
 	}
-	reversed = word.split("").reverse().join("");
-	
+
+	reversed = word.split("").reverse().join("");	//split- separate letters into array, reverse array, join letters back into string
+
 	if (word === reversed){
 		return true;
 	}
--- Andelabs/reverse string lab/tests.js
+++ Andelabs/reverse string lab/tests.js
@@ -38,7 +38,56 @@ describe("Produce the reverse order of a word: ", function() {
     it("should return `csim` for `misc`", function() {
       expect(app.reverseString('misc')).toEqual('csim');
     });
+  });
+
+  describe("Case for non-string data type", function() {
+
+    it("should return 'false argument' for 123", function() {
+      expect(app.reverseString(123)).toEqual('false argument');
+    });
+
+    it("should return 'false argument' boolean true", function() {
+      expect(app.reverseString(true)).toEqual('false argument');
+    });
+  });
+
+  describe("Case for strings not necessarily words", function() {
+
+    it("should return `13452` for `25431`", function() {
+      expect(app.reverseString('25431')).toEqual('13452');
+    });
+  });  
+
+  describe("Case for palindrome-string which is not necessarily a word", function() {
+
+    it("should return true for `323`", function() {
+      expect(app.reverseString('323')).toEqual(true);
+    });
+
+    it("should return true for `777`", function() {
+      expect(app.reverseString('777')).toEqual(true);
+    });
+
+    it("should return true for `ab8ba`", function() {
+      expect(app.reverseString('ab8ba')).toEqual(true);
+    });
+  });
+
+  describe("Case for case-sensitivity", function() {
+
+    it("should return `Aaa` for `aaA`", function() {
+      expect(app.reverseString('aaA')).toEqual('Aaa');
+    });
+
+    it("should return `aBba` for `abBa`", function() {
+      expect(app.reverseString('abBa')).toEqual('aBba');
+    });
+
+    it("should return `true` for `mAllAm`", function() {
+      expect(app.reverseString('mAllAm')).toEqual(true);
+    });
 
   });
+
 
 });
